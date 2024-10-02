48c63973d44a1b8487dca9f4b2f3bfce9423db35
Erivelton Matos <eriveltonmatos@hotmail.com>
2024-10-01 22:05:47 -0300

    first commit

---
+++ app/work/page.tsx
@@ -0,0 +1,3 @@
+export default function Work() {
+  return <h1>work Page</h1>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,60 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrains-mono)", // Nome da variável corrigido
+    },
+    extend: {
+
+
+	  keyframes: {
+		"accordion-down": {
+			from: { height: "0"},
+			to: { height: "var(--radix-accordion-content-height)"},
+		},
+		"accordion-up": {
+			to: { height: "var(--radix-accordion-content-height)"},
+			from: { height: "0"},
+		},
+	  },
+	  animation: {
+		"accordion-down": "accordion-down 0.2s ease-out",
+		"accordion-up": "accordion-up 0.2s ease-out",
+	  },
+      colors: {
+		primary: '#1c1c22',
+		accent: {
+			DEFAULT: '#00ff99',
+			hover: '#00e187',
+		},
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
